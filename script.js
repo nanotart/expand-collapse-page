1badbd1b9c44ff1ebe9db624c03514abb7ce412d
nanotart <shway2020@gmail.com>
2022-08-04 11:10:18 -0700

    optimization

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ document.addEventListener('click', e => {
     if (!e.target.matches('.expand-button')) return 
 
     // store the original flag
-    let original_flag = e.target.innerHTML
+    // let original_flag = e.target.innerHTML
 
     // change flag, identify parent elements
     e.target.innerHTML = changeButton(e.target.innerHTML)
     const card = e.target.closest('.card')
     const cardContent = card.querySelector('.card-body')
 
-    if (original_flag === "Collapse") {
-        cardContent.classList.remove('show')
-    } else {
-        cardContent.classList.add('show')
-    }
+    // if (original_flag === "Collapse") {
+    //     cardContent.classList.remove('show')
+    // } else {
+    //     cardContent.classList.add('show')
+    // }
+
+    // this basically shortens the code from 4 lines to 1
+    cardContent.classList.toggle('show')
 })
 
 // Changes the button wording
