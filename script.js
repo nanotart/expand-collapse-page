30ec825160ea17115155647d6ab46437a0d6a22a
nanotart <shway2020@gmail.com>
2022-08-04 11:03:43 -0700

    added flag capabilities and finished collapse/expand

---
+++ script.js
@@ -0,0 +1,25 @@
+// Code for Expanding and Collapsing
+document.addEventListener('click', e => {
+    // if button is not pressed, simply return
+    if (!e.target.matches('.expand-button')) return 
+
+    // store the original flag
+    let original_flag = e.target.innerHTML
+
+    // change flag, identify parent elements
+    e.target.innerHTML = changeButton(e.target.innerHTML)
+    const card = e.target.closest('.card')
+    const cardContent = card.querySelector('.card-body')
+
+    if (original_flag === "Collapse") {
+        cardContent.classList.remove('show')
+    } else {
+        cardContent.classList.add('show')
+    }
+})
+
+// Changes the button wording
+function changeButton(flag) {
+    if (flag === "Expand") return "Collapse"
+    return "Expand"
+}
